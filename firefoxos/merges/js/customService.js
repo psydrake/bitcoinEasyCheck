3d823bc4ce735d9e7aecec44e1f9dc7eb43db15a
Drake Emko <demko@revionics.com>
2013-12-31 02:38:47 -0700

    fixed tracking code for firefoxos

--- firefoxos/merges/js/customService.js
+++ firefoxos/merges/js/customService.js
@@ -12,7 +12,7 @@ angular.module('app.customService', []).
 				  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 				  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-				  ga('create', 'UA-46128370-3', 'd398aqb20c16vz.cloudfront.net');
+				  ga('create', 'UA-46128370-3', 'd23qh8rj19dgra.cloudfront.net');
 				  ga('send', 'pageview', {
 					'page': page
 				  });
