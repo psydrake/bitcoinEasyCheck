c3d0efac51b3bdb9ee9a83bfd9221a11eddd1893
Drake Emko <demko@revionics.com>
2014-03-13 18:41:13 -0600

    reversed swipe directions for screen navigation

--- www/js/app.js
+++ www/js/app.js
@@ -9,6 +9,7 @@ var app = angular.module('app', [
 		'ngTouch',
 		//'ngCookies',
         'ui.bootstrap'
+		//'mgcrea.pullToRefresh'
     ]).
 	config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
